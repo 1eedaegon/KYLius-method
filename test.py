6ed24185a1aa0d62fd1496c50f9782ea3f120c78
Kim Seunghyuck <33567817+smallerhand@users.noreply.github.com>
2018-04-03 01:32:38 +0900

    Create test.py

---
+++ test.py
@@ -0,0 +1,16 @@
+#this is nothing. don't care about this.
+#I'm writing down this just because there is no code at all.
+
+class Goodman:
+  def __init__(w):
+    w.value = 0
+  def add(w, x):
+    w.value += x
+
+a=Goodman()
+a.add(1)
+a.add(2)
+a.add(5)
+print(a.value)
+
+print('happy days!')

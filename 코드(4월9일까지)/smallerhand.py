b977bfe99484113d157cf3dfd04d89f26f15670f
smallerhand <mrmu0124@gmail.com>
2018-04-09 09:09:05 +0900

    all

--- 코드(4월9일까지)/smallerhand.py
+++ 코드(4월9일까지)/smallerhand.py
@@ -8,13 +8,15 @@ Created on Sun Apr  8 15:23:47 2018
 ## 개선할 사항: layer수 늘리기, weight 초기값 xavier(오류나서 못했음), activation function 은 maxout을 쓰자. 
 ## dropout(앙상블). adam optimizer
 import tensorflow as tf
-import random
 import pandas as pd
 import numpy as np
-tf.set_random_seed(777)  # reproducibility
 train = pd.read_csv('desktop/python/train.csv')
 test = pd.read_csv('desktop/python/test.csv')
-trainData = train.values[:,1:]
+#훈련세트, validation세트 나누기
+from sklearn.model_selection import train_test_split
+train_set, validate_set = train_test_split(train, test_size = 0.3)
+trainData = train_set.values[:,1:]
+validateData = validate_set.values[:,1:]
 #one hot function(n=10)
 def one_hot(x):
     lst=[]
@@ -22,7 +24,8 @@ def one_hot(x):
         lst.append([0,0,0,0,0,0,0,0,0,0])
         lst[-1][i]+=1
     return(lst)
-trainLabel=one_hot(train.values[:,0])
+trainLabel=one_hot(train_set.values[:,0])
+validateLabel=one_hot(validate_set.values[:,0])
 # parameters
 learning_rate = 0.001
 training_epochs = 20
@@ -51,7 +54,7 @@ hypothesis = tf.matmul(L3, W4) + b4
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
     logits=hypothesis, labels=Y))
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
-# initialize
+# initialize 
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 # train my model
@@ -66,27 +69,9 @@ for epoch in range(training_epochs):
         avg_cost += c / total_batch
     print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.9f}'.format(avg_cost))
 print('Learning Finished!')
-saver = tf.train.Saver()
-saver.save(sess, 'desktop/git/KYLius-method/save')
 
-#0.941~0.9461보다 높길.
-loaded_Graph = tf.Graph()
-with tf.Session(graph=loaded_Graph) as sess:
-    loader = tf.train.import_meta_graph('desktop/git/KYLius-method/save.meta')
-    loader.restore(sess, 'desktop/git/KYLius-method/save.meta')    
-    # get tensors
-    loaded_x = loaded_Graph.get_tensor_by_name('input:0')
-    loaded_y = loaded_Graph.get_tensor_by_name('label:0')
-    loaded_prob = loaded_Graph.get_tensor_by_name('probability:0')
-    prob = sess.run(tf.argmax(loaded_prob,1), feed_dict = {loaded_x: testData})
-
-import csv
-header = ['ImageID','Label']
-with open('desktop/git/KYLius-method/output_smallerhand.csv', 'w') as csvFile:
-    writer = csv.writer(csvFile, delimiter = ',')
-    writer.writerow(header)
-    for i, p in enumerate(prob):
-        writer.writerow([str(i+1), str(p)])
-
-    
-    
+# Test model and check accuracy(validation)
+correct_prediction = tf.equal(tf.argmax(hypothesis, 1), tf.argmax(Y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print('Accuracy:', sess.run(accuracy, feed_dict={
+      X: validateData, Y: validateLabel}))

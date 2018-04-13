d2b91ed178d22c50c29fefb782543091683f6acc
smallerhand <mrmu0124@gmail.com>
2018-04-13 17:36:52 +0900

    all

--- 코드/smallerhand_cnn.py
+++ 코드/smallerhand_cnn.py
@@ -51,7 +51,7 @@ L2 = tf.nn.relu(L2)
 L2 = tf.nn.max_pool(L2, ksize=[1, 2, 2, 1],
                     strides=[1, 2, 2, 1], padding='SAME')
 L2 = tf.nn.dropout(L2, keep_prob=keep_prob)
-L2_flat = tf.reshape(L1, [-1, 7 * 7 * 64])
+L2_flat = tf.reshape(L2, [-1, 7 * 7 * 64])
 
 # L3
 W3 = tf.Variable(tf.random_normal([3, 3, 64, 128], stddev=0.01))
@@ -63,14 +63,17 @@ L3 = tf.nn.dropout(L3, keep_prob=keep_prob)
 L3_flat = tf.reshape(L3, [-1, 4 * 4 * 128])
 
 # Final FC 7x7x64 inputs -> 10 outputs
+"""
 W4_1 = tf.get_variable("W4_1", shape=[14 * 14 * 32, 10],
                      initializer=tf.contrib.layers.xavier_initializer())
+"""
 W4_2 = tf.get_variable("W4_2", shape=[7 * 7 * 64, 10],
                      initializer=tf.contrib.layers.xavier_initializer())
 W4_3 = tf.get_variable("W4_3", shape=[4 * 4 * 128, 10],
                      initializer=tf.contrib.layers.xavier_initializer())
 b = tf.Variable(tf.random_normal([10]))
-logits = tf.matmul(L1_flat, W4_1) + tf.matmul(L2_flat, W4_2) + tf.matmul(L3_flat, W4_3) + b
+#logits = tf.matmul(L1_flat, W4_1) + tf.matmul(L2_flat, W4_2) + tf.matmul(L3_flat, W4_3) + b
+logits = tf.matmul(L2_flat, W4_2) + tf.matmul(L3_flat, W4_3) + b
 
 # define cost/loss & optimizer
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(

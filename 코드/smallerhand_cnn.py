6d766761f61e31dc93c4b6d7cb8a3395f693967b
smallerhand <mrmu0124@gmail.com>
2018-04-13 17:54:28 +0900

    all

--- 코드/smallerhand_cnn.py
+++ 코드/smallerhand_cnn.py
@@ -44,13 +44,12 @@ L1 = tf.nn.max_pool(L1, ksize=[1, 2, 2, 1],
 L1 = tf.nn.dropout(L1, keep_prob=keep_prob)
 
 # L1_2
-W1_2 = tf.Variable(tf.random_normal([5, 5, 1, 32], stddev=0.01))
+W1_2 = tf.Variable(tf.random_normal([3, 3, 1, 32], stddev=0.01))
 L1_2 = tf.nn.conv2d(X_img, W1_2, strides=[1, 1, 1, 1], padding='SAME')
 L1_2 = tf.nn.relu(L1)
-L1_2 = tf.nn.max_pool(L1_2, ksize=[1, 7, 7, 1],
-                    strides=[1, 7, 7, 1], padding='SAME')
+L1_2 = tf.nn.max_pool(L1_2, ksize=[1, 2, 2, 1],
+                    strides=[1, 2, 2, 1], padding='SAME')
 L1_2 = tf.nn.dropout(L1_2, keep_prob=keep_prob)
-L1_2_flat = tf.reshape(L1_2, [-1, 4 * 4 * 32])
 
 # L2 ImgIn shape=(?, 14, 14, 10)
 W2 = tf.Variable(tf.random_normal([3, 3, 32, 64], stddev=0.01))
@@ -59,6 +58,14 @@ L2 = tf.nn.relu(L2)
 L2 = tf.nn.max_pool(L2, ksize=[1, 2, 2, 1],
                     strides=[1, 2, 2, 1], padding='SAME')
 L2 = tf.nn.dropout(L2, keep_prob=keep_prob)
+
+# L2_2
+W2_2 = tf.Variable(tf.random_normal([3, 3, 32, 64], stddev=0.01))
+L2_2 = tf.nn.conv2d(L1_2, W2_2, strides=[1, 1, 1, 1], padding='SAME')
+L2_2 = tf.nn.relu(L2_2)
+L2_2 = tf.nn.max_pool(L2_2, ksize=[1, 2, 2, 1],
+                    strides=[1, 2, 2, 1], padding='SAME')
+L2_2 = tf.nn.dropout(L2_2, keep_prob=keep_prob)
 
 # L3
 W3 = tf.Variable(tf.random_normal([3, 3, 64, 128], stddev=0.01))
@@ -69,13 +76,22 @@ L3 = tf.nn.max_pool(L3, ksize=[1, 2, 2, 1],
 L3 = tf.nn.dropout(L3, keep_prob=keep_prob)
 L3_flat = tf.reshape(L3, [-1, 4 * 4 * 128])
 
+# L3_2
+W3_2 = tf.Variable(tf.random_normal([3, 3, 64, 128], stddev=0.01))
+L3_2 = tf.nn.conv2d(L2_2, W3_2, strides=[1, 1, 1, 1], padding='SAME')
+L3_2 = tf.nn.relu(L3_2)
+L3_2 = tf.nn.max_pool(L3_2, ksize=[1, 2, 2, 1],
+                    strides=[1, 2, 2, 1], padding='SAME')
+L3_2 = tf.nn.dropout(L3_2, keep_prob=keep_prob)
+L3_2_flat = tf.reshape(L3_2, [-1, 4 * 4 * 128])
+
 # Final FC 7x7x64 inputs -> 10 outputs
-W4_2 = tf.get_variable("W4_2", shape=[4 * 4 * 32, 10],
+W4_1 = tf.get_variable("W4_1", shape=[4 * 4 * 128, 10],
                      initializer=tf.contrib.layers.xavier_initializer())
-W4_3 = tf.get_variable("W4_3", shape=[4 * 4 * 128, 10],
+W4_2 = tf.get_variable("W4_2", shape=[4 * 4 * 128, 10],
                      initializer=tf.contrib.layers.xavier_initializer())
 b = tf.Variable(tf.random_normal([10]))
-logits = tf.matmul(L1_2_flat, W4_2) + tf.matmul(L3_flat, W4_3) + b
+logits = tf.matmul(L3_flat, W4_1) + tf.matmul(L3_2_flat, W4_2) + b
 
 # define cost/loss & optimizer
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
@@ -115,4 +131,5 @@ batch_size = 100
 2. 상욱이가 올린 김성훈교수 코드 인수 넣음.
 98.77%(epoch 5)
 98.94%(epoch 20)
+99.10%(epoch 20)
 """

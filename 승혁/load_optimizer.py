30027be284784ecaf70caa7052464063f9fdf936
smallerhand <mrmu0124@gmail.com>
2018-04-18 01:48:30 +0900

    all

--- 승혁/load_optimizer.py
+++ 승혁/load_optimizer.py
@@ -1,20 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 18 01:33:49 2018
+
+@author: kimseunghyuck
+"""
+
 import tensorflow as tf
 import numpy as np
-tf.reset_default_graph()     #그래프 초기화
-tf.set_random_seed(777) 
-import pandas as pd
 path="/home/paperspace/Downloads/"
-#train = pd.read_csv('c:/python/train.csv')
+
+#데이터 가져오기
 train = pd.read_csv(path+"train.csv')
 #train = pd.read_csv('/home/itwill03/다운로드/train.csv')
 
-#훈련세트, validation세트 나누기
+#훈련세트, validation세트 나누기(여기서는 validate만 필요)
 from sklearn.model_selection import train_test_split
 train_set, validate_set = train_test_split(train, test_size = 0.3)
-trainData = train_set.values[:,1:]
+#trainData = train_set.values[:,1:]
 validateData = validate_set.values[:,1:]
-trainLabel=train_set.values[:,0]
+#trainLabel=train_set.values[:,0]
 validateLabel=validate_set.values[:,0]
+
 
 X = tf.placeholder(tf.float32, [None, 784])
 X_img = tf.reshape(X, [-1, 28, 28, 1])          # img 28x28x1 (black/white)
@@ -73,33 +80,11 @@ W_o = tf.get_variable("W_o", shape=[625,10],initializer=tf.contrib.layers.xavier
 b = tf.Variable(tf.random_normal([10]))
 logits = tf.matmul(L4, W_o) + b
 
-# define cost/loss & optimizer
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits,labels= Y_onehot))
-#optimizer = tf.train.RMSPropOptimizer(0.001, 0.9).minimize(cost)
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost) # 아담버젼
-predict_op = tf.argmax(logits, 1)
-
-# initialize
-sess = tf.Session()
-sess.run(tf.global_variables_initializer())
-
-# train my model
-print('Learning started. It takes sometime.')
-for epoch in range(training_epochs):
-    avg_cost = 0
-    total_batch = int(len(trainData) / batch_size)
-    for i in range(total_batch):
-        batch_xs = trainData[i*batch_size:(i+1)*batch_size]
-        batch_ys = trainLabel[i*batch_size:(i+1)*batch_size].reshape(-1, 1)
-        feed_dict = {X: batch_xs, Y: batch_ys, p_keep_conv: .7, p_keep_hidden: .5}
-        c, _ = sess.run([cost, optimizer], feed_dict=feed_dict)
-        avg_cost += c / total_batch
-    print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.9f}'.format(avg_cost))
-    if epoch % steps_for_validate == steps_for_validate-1:
-        correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(Y_onehot, 1))
-        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-        print('Accuracy:', sess.run(accuracy, feed_dict={
-                X: validateData, Y: validateLabel.reshape(-1, 1), p_keep_conv: 1, p_keep_hidden: 1}))
-        saver = tf.train.Saver()
-        save_path = saver.save(sess, path+'opt.ckpt')
-print('Finished!')
+#train 한 옵티마이저와서 맞추기
+with tf.Session() as sess:
+    saver.restore(sess, path+"opt.ckpt")
+    print("Model restored.")
+    correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(Y_onehot, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    print('Accuracy:', sess.run(accuracy, feed_dict={
+            X: validateData, Y: validateLabel.reshape(-1, 1), p_keep_conv: 1, p_keep_hidden: 1}))  

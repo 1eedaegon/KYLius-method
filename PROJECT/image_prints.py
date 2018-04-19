66586d5920b541735a07b497517fbcf69e6b7f3e
suwonyu <37686127+suwonyu@users.noreply.github.com>
2018-04-19 17:39:25 +0900

    Add files via upload

--- PROJECT/image_prints.py
+++ PROJECT/image_prints.py
@@ -8,15 +8,28 @@ Created on Thu Apr 19 12:07:50 2018
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
-image_data = cv2.imread('/data/data3.png')
-image_data = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+train = pd.read_csv('/data/mnist/train.csv')
+
+from sklearn.model_selection import train_test_split
+train_set, validate_set = train_test_split(train, test_size = 0.3)
+trainData = train_set.values[:,1:]
+validateData = validate_set.values[:,1:]
+trainLabel=train_set.values[:,0]
+validateLabel=validate_set.values[:,0]
+
+img = trainData
+label = trainLabel
+
+image = cv2.imread('/data/data3.png')
+image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 
 #single image printing function
     #im_num : number of image , size : output_size 
 def sing_prt(im_num, size):
     fig1, axis = plt.subplots(1,1, figsize=(size,size))
-    axis.imshow(img[0].reshape(28,28))
+    axis.imshow(img[im_num].reshape(28,28))
 
 sing_prt(5,2)
 
@@ -47,12 +60,11 @@ def conv_prt(start,end,strides,num,size):
     fig1, axis = plt.subplots(1,number, figsize=(size,size))
     for i in range(number):
         axis[i].imshow(image[st:en,st:en])
-        axis[i].axis('off')
-        axis[i].set_title(label[i])
+        axis[i].axis('off')      
         st += strides
         en += strides
         
-conv_prt(0,100,5,5,10)    
+conv_prt(0,100,5,10,10)    
 
 #images printing function (operated on weight values) 
     # start,end : 이미지 행렬 좌표의 시작점, 끝점
@@ -65,14 +77,14 @@ def conv_prt(start,end,w,size):
     st = start
     en = end
         
-    fig1, axis = plt.subplots(1,number, figsize=(size,size))
+    fig1, axis = plt.subplots(1,w, figsize=(size,size))
     for i in range(w):
         axis[i].imshow(image[st:en,st:en]*(i+1))
         axis[i].axis('off')
-        axis[i].set_title(label[i])
+        
 
         
-conv_prt(0,100,5,5,10)    
+conv_prt(0,100,5,5)    
 
 
 

29e8f40c32896ecaf2dea354360f3b2781ad3cf4
smallerhand <mrmu0124@gmail.com>
2018-04-19 10:28:39 +0900

    all

--- 승혁/img_to_csv.py
+++ 승혁/img_to_csv.py
@@ -5,10 +5,13 @@ Created on Wed Apr 18 00:50:27 2018
 
 @author: kimseunghyuck
 """
-import sys
-sys.path
-
 from PIL import Image
 import numpy
-img=Image.open("/Users/kimseunghyuck/desktop/number1.jpg")
-imgarray=numpy.array(img)
+img=Image.open("/Users/kimseunghyuck/desktop/git/daegon/KYLius-method/승혁/numbers_image/numbers1.jpg")
+size=(28,28)
+#사진을 흑백으로 바꿈
+im2=img.convert("L")
+#사진 사이즈를 28*28로 바꿈.
+im2=im2.resize(size)
+#csv로 변환
+imgarray=numpy.array(im2)

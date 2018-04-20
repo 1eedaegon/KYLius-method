cf021d048e30b043df481923464838c194d18c59
kwak <ksh6452@gmail.com>
2018-04-20 15:29:57 +0900

    update

---
+++ PROJECT/기본코드정리.py
@@ -0,0 +1,93 @@
+import tensorflow as tf
+import numpy as np
+tf.reset_default_graph()     #그래프 초기화
+tf.set_random_seed(777) 
+import pandas as pd
+
+#train = pd.read_csv('c:/python/train.csv')
+train = pd.read_csv('/home/itwill03/다운로드/train.csv')
+
+#훈련세트, validation세트 나누기
+from sklearn.model_selection import train_test_split
+train_set, validate_set = train_test_split(train, test_size = 0.3)
+trainData = train_set.values[:,1:]
+validateData = validate_set.values[:,1:]
+trainLabel=train_set.values[:,0]
+validateLabel=validate_set.values[:,0]
+
+X = tf.placeholder(tf.float32, [None, 784])
+X_img = tf.reshape(X, [-1, 28, 28, 1])          # img 28x28x1 (black/white)
+Y = tf.placeholder(tf.int32, [None, 1])
+Y_onehot=tf.reshape(tf.one_hot(Y, 10), [-1, 10])
+p_keep_conv = tf.placeholder(tf.float32)
+p_keep_hidden = tf.placeholder(tf.float32)
+
+# hyper parameters
+learning_rate = 0.001
+training_epochs = 20
+batch_size = 100
+steps_for_validate = 5
+keep_prob = tf.placeholder(tf.float32)
+
+# L1 ImgIn shape=(?, 28, 28, 1)
+W1 = tf.Variable(tf.random_normal([3, 3, 1, 32], stddev=0.01))
+L1 = tf.nn.conv2d(X_img, W1, strides=[1, 1, 1, 1], padding='SAME')
+L1 = tf.nn.relu(L1)
+L1 = tf.nn.max_pool(L1, ksize=[1, 2, 2, 1],strides=[1, 2, 2, 1], padding='SAME') # l1 shape=(?, 14, 14, 32)
+
+
+# L2 ImgIn shape=(?, 14, 14, 10)
+W2 = tf.Variable(tf.random_normal([3, 3, 32, 64], stddev=0.01))
+L2 = tf.nn.conv2d(L1, W2, strides=[1, 1, 1, 1], padding='SAME')
+L2 = tf.nn.relu(L2)
+L2 = tf.nn.max_pool(L2, ksize=[1, 2, 2, 1],strides=[1, 2, 2, 1], padding='SAME') # l2 shape=(?, 7, 7, 64)
+
+
+# L3 ImgIn shape=(?, 7, 7, 128)
+W3 = tf.Variable(tf.random_normal([3, 3, 64, 128], stddev=0.01))
+L3 = tf.nn.conv2d(L2, W3, strides=[1, 1, 1, 1], padding='SAME')
+L3 = tf.nn.relu(L3)
+L3 = tf.nn.max_pool(L3, ksize=[1, 2, 2, 1],strides=[1, 2, 2, 1], padding='SAME') # l3 shape=(?, 4, 4, 128)
+L3_flat = tf.reshape(L3, shape=[-1, 128 * 4 * 4])    # reshape to (?, 2048)
+
+
+# Final FC 4x4x128 inputs -> 10 outputs
+W4 = tf.Variable(tf.random_normal([128 * 4 * 4, 625], stddev=0.01))
+L4 = tf.nn.relu(tf.matmul(L3_flat, W4))
+W_o = tf.get_variable("W_o", shape=[625,10],initializer=tf.contrib.layers.xavier_initializer())
+b = tf.Variable(tf.random_normal([10]))
+logits = tf.matmul(L4, W_o) + b
+
+# define cost/loss & optimizer
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits,labels= Y_onehot))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost) # 아담버젼
+predict_op = tf.argmax(logits, 1)
+
+# initialize
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+saver = tf.train.Saver()
+
+# train my model
+print('Learning started. It takes sometime.')
+for epoch in range(training_epochs):
+    avg_cost = 0
+    total_batch = int(len(trainData) / batch_size)
+    for i in range(total_batch):
+        batch_xs = trainData[i*batch_size:(i+1)*batch_size]
+        batch_ys = trainLabel[i*batch_size:(i+1)*batch_size].reshape(-1, 1)
+        feed_dict = {X: batch_xs, Y: batch_ys, p_keep_conv: .7, p_keep_hidden: .5}
+        c, _ = sess.run([cost, optimizer], feed_dict=feed_dict)
+        avg_cost += c / total_batch
+    print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.9f}'.format(avg_cost))
+    if epoch % steps_for_validate == steps_for_validate-1:
+        correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(Y_onehot, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        print('Accuracy:', sess.run(accuracy, feed_dict={
+                X: validateData, Y: validateLabel.reshape(-1, 1), p_keep_conv: 1, p_keep_hidden: 1}))       
+print('Finished!')
+
+saver.save(sess, '/home/itwill03/다운로드/cnn_session')
+save_path = saver.save(sess, "/home/itwill03/다운로드//opt2")
+
+sess.close()

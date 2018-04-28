f5c60142d7d857a5c6ef6700f152ab63e4b3ff85
곽상욱 <37833373+Trigger21@users.noreply.github.com>
2018-04-29 01:02:34 +0900

    Create Preparing data.py

---
+++ x_ksw/New Project/Preparing data.py
@@ -0,0 +1,48 @@
+class Config(object):
+    def __init__(self,
+                 sampling_rate=16000, audio_duration=2, n_classes=41,
+                 use_mfcc=False, n_folds=10, learning_rate=0.0001, 
+                 max_epochs=50, n_mfcc=20):
+        self.sampling_rate = sampling_rate
+        self.audio_duration = audio_duration
+        self.n_classes = n_classes
+        self.use_mfcc = use_mfcc
+        self.n_mfcc = n_mfcc
+        self.n_folds = n_folds
+        self.learning_rate = learning_rate
+        self.max_epochs = max_epochs
+
+        self.audio_length = self.sampling_rate * self.audio_duration
+        if self.use_mfcc:
+            self.dim = (self.n_mfcc, 1 + int(np.floor(self.audio_length/512)), 1)
+        else:
+            self.dim = (self.audio_length, 1)
+
+config = Config(sampling_rate=44100, audio_duration=2, n_folds=10, 
+                learning_rate=0.001, use_mfcc=True, n_mfcc=40)
+
+def prepare_data(df, config, data_dir):
+    X = np.empty(shape=(df.shape[0], config.dim[0], config.dim[1], 1))
+    input_length = config.audio_length
+    for i, fname in enumerate(df['fname']):
+        print(fname)
+        file_path = data_dir + fname
+        data, _ = librosa.core.load(file_path, sr=config.sampling_rate, res_type="kaiser_fast")
+
+        # Random offset / Padding
+        if len(data) > input_length:
+            max_offset = len(data) - input_length
+            offset = np.random.randint(max_offset)
+            data = data[offset:(input_length+offset)]
+        else:
+            if input_length > len(data):
+                max_offset = input_length - len(data)
+                offset = np.random.randint(max_offset)
+            else:
+                offset = 0
+            data = np.pad(data, (offset, input_length - len(data) - offset), "constant")
+
+        data = librosa.feature.mfcc(data, sr=config.sampling_rate, n_mfcc=config.n_mfcc)
+        data = np.expand_dims(data, axis=-1)
+        X[i,] = data
+    return X

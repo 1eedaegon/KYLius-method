aa207094215f65d54a401fe487b41ad7a30b011d
smallerhand <mrmu0124@gmail.com>
2018-05-01 13:47:30 +0900

    all

--- x_ksh/batch2_ps.py
+++ x_ksh/batch2_ps.py
@@ -27,7 +27,7 @@ tf.reset_default_graph()     #그래프 초기화
 # hyper parameters
 learning_rate = 0.0002
 training_epochs = 700
-batch_size = 300
+batch_size = 50
 steps_for_validate = 5
 
 #placeholder
@@ -38,9 +38,14 @@ Y_onehot=tf.reshape(tf.one_hot(Y, 41), [-1, 41])
 p_keep_conv = tf.placeholder(tf.float32, name="p_keep_conv")
 p_keep_hidden = tf.placeholder(tf.float32, name="p_keep_hidden")
 
+# test flag for batch normalization
+tst = tf.placeholder(tf.bool)
+iter = tf.placeholder(tf.int32)
+
 # L1 SoundIn shape=(?, 20, 430, 1)
 W1 = tf.get_variable("W1", shape=[2, 43, 1, 32],initializer=tf.contrib.layers.xavier_initializer())
 L1 = tf.nn.conv2d(X_sound, W1, strides=[1, 1, 1, 1], padding='SAME')
+L1 = tf.layers.batch_normalization(L1)
 L1 = tf.nn.elu(L1)
 L1 = tf.nn.max_pool(L1, ksize=[1, 2, 43, 1],strides=[1, 2, 43, 1], padding='SAME') 
 L1 = tf.nn.dropout(L1, p_keep_conv)
@@ -48,6 +53,7 @@ L1 = tf.nn.dropout(L1, p_keep_conv)
 # L2 Input shape=(?,10,21,32)
 W2 = tf.get_variable("W2", shape=[3, 3, 32, 64],initializer=tf.contrib.layers.xavier_initializer())
 L2 = tf.nn.conv2d(L1, W2, strides=[1, 1, 1, 1], padding='SAME')
+L2 = tf.layers.batch_normalization(L2)
 L2 = tf.nn.elu(L2)
 L2 = tf.nn.max_pool(L2, ksize=[1, 2, 2, 1],strides=[1, 2, 2, 1], padding='SAME') 
 L2 = tf.nn.dropout(L2, p_keep_conv)
@@ -55,16 +61,17 @@ L2 = tf.nn.dropout(L2, p_keep_conv)
 # L3 Input shape=(?,3,12,64)
 W3 = tf.get_variable("W3", shape=[3, 3, 64, 128],initializer=tf.contrib.layers.xavier_initializer())
 L3 = tf.nn.conv2d(L2, W3, strides=[1, 1, 1, 1], padding='SAME')
+L3 = tf.layers.batch_normalization(L3)
 L3 = tf.nn.elu(L3)
 L3 = tf.nn.max_pool(L3, ksize=[1, 2, 2, 1],strides=[1, 2, 2, 1], padding='SAME') 
 L3 = tf.nn.dropout(L3, p_keep_conv)
 L3_flat= tf.reshape(L3, shape=[-1, 3*3*128])
 
 # Final FC 2*3*128 inputs -> 41 outputs
-W4 = tf.get_variable("W4", shape=[3*3*128, 625],initializer=tf.contrib.layers.xavier_initializer())
+W4 = tf.get_variable("W4", shape=[3*3*128, 512],initializer=tf.contrib.layers.xavier_initializer())
 L4 = tf.nn.elu(tf.matmul(L3_flat, W4))
 L4 = tf.nn.dropout(L4, p_keep_hidden)
-W_o = tf.get_variable("W_o", shape=[625,41],initializer=tf.contrib.layers.xavier_initializer())
+W_o = tf.get_variable("W_o", shape=[512,41],initializer=tf.contrib.layers.xavier_initializer())
 b = tf.Variable(tf.random_normal([41]))
 logits = tf.matmul(L4, W_o) + b
 
@@ -86,7 +93,8 @@ for epoch in range(training_epochs):
     for i in range(total_batch):
         batch_xs = trainData[i*batch_size:(i+1)*batch_size]
         batch_ys = trainLabel[i*batch_size:(i+1)*batch_size].reshape(-1, 1)
-        feed_dict = {X: batch_xs, Y: batch_ys, p_keep_conv: .8, p_keep_hidden: 0.7}
+        feed_dict = {X: batch_xs, Y: batch_ys, 
+                     p_keep_conv: .8, p_keep_hidden: 0.7}
         c, _ = sess.run([cost, optimizer], feed_dict=feed_dict)
         avg_cost += c / total_batch
     print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.9f}'.format(avg_cost))
@@ -95,7 +103,8 @@ for epoch in range(training_epochs):
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
         x=np.random.choice(testLabel.shape[0], 500, replace=False)
         print('Accuracy:', sess.run(accuracy, feed_dict={
-                X: testData[x], Y: testLabel[x].reshape(-1, 1), p_keep_conv: 1, p_keep_hidden: 1}))
+                X: testData[x], Y: testLabel[x].reshape(-1, 1), 
+                p_keep_conv: 1, p_keep_hidden: 1}))
         save_path = saver.save(sess, '/home/paperspace/Downloads/optx/optx')
 print('Finished!')
 
@@ -121,11 +130,4 @@ accuracy : 66~74%
 lr=0.0002, epoch = 700
 p_keep_conv, p_keep_hidden = 0.7, 0.5
 accuracy : 67~72%
-
-*다른 시도: batch_norm 해본거 SA_batch_norm_ps3.py - 인자값이 잘못됐는지 정확도가 엄청 떨어짐
-
-12) 9와 같고
-win : (2, 43), (3,3), (3,3)
-max_pool : (2,43), (2,2), (2,2)
-accuracy : 
 """

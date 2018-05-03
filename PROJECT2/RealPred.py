cd2a6c0975b02baaad905a8250533f5d58a3abf8
smallerhand <mrmu0124@gmail.com>
2018-05-04 02:49:26 +0900

    all

--- PROJECT2/RealPred.py
+++ PROJECT2/RealPred.py
@@ -1,6 +1,5 @@
 import numpy as np
-def predict(array1, array2, array3):
-    idx = np.array(['Hi-hat', 'Saxophone', 'Trumpet', 'Glockenspiel', 'Cello', 'Knock',
+idx = np.array(['Hi-hat', 'Saxophone', 'Trumpet', 'Glockenspiel', 'Cello', 'Knock',
        'Gunshot_or_gunfire', 'Clarinet', 'Computer_keyboard',
        'Keys_jangling', 'Snare_drum', 'Writing', 'Laughter', 'Tearing',
        'Fart', 'Oboe', 'Flute', 'Cough', 'Telephone', 'Bark', 'Chime',
@@ -9,20 +8,6 @@ def predict(array1, array2, array3):
        'Shatter', 'Fireworks', 'Tambourine', 'Cowbell', 'Electric_piano',
        'Meow', 'Drawer_open_or_close', 'Applause', 'Acoustic_guitar',
        'Violin_or_fiddle', 'Finger_snapping'])
-    array=array1+array2+array3
-    predarray=np.argmax(array, axis=1)
-
-array1=np.genfromtxt('/Users/kimseunghyuck/desktop/array1.csv', delimiter=',')
-array2=np.genfromtxt('/Users/kimseunghyuck/desktop/array2.csv', delimiter=',')
-array3=np.genfromtxt('/Users/kimseunghyuck/desktop/array3.csv', delimiter=',')
-
-
-print(array1.shape, array2.shape, array3.shape)
-
-
-array=0.77*array1+0.64*array2+0.76*array3
-
-predarray=np.argmax(array, axis=1)
 
 path='/Users/kimseunghyuck/desktop/'
 import os
@@ -30,8 +15,29 @@ import os
 file_list=os.listdir(path+'audio_test')
 file_list=np.array(file_list).reshape(-1)
 
+#각각의 소프트맥스 데이터를 불러옴.
+array1=np.genfromtxt('/Users/kimseunghyuck/desktop/array1.csv', delimiter=',')
+array2=np.genfromtxt('/Users/kimseunghyuck/desktop/array2.csv', delimiter=',')
+array3=np.genfromtxt('/Users/kimseunghyuck/desktop/array3.csv', delimiter=',')
+print(array1.shape, array2.shape, array3.shape)
 
+#가중치를 곱하여 더한 후 argmax
+array=0.77/2*array1+0.64*array2+0.76/2*array3
+predarray=np.argmax(array, axis=1)
 
+print(file_list.shape, predarray.shape)
 
+import pandas as pd
+
+submission={}
+i=0
+for file in file_list:
+    if (file.split('.')[-1]=="wav"):
+        submission[file]=idx[predarray[i]]
+    i+=1
+
+#파일 아웃풋
+KYLius5=pd.DataFrame([[k,v] for k,v in iter(submission.items())],columns=["fname","label"])
+KYLius5.to_csv(path+'KYLius5.csv', header=True, index=False, sep='\t')
 
 

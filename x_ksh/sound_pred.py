64703271b525f0b1fcfdad2c76d0b36d538a6e01
smallerhand <mrmu0124@gmail.com>
2018-05-03 19:33:54 +0900

    all

--- x_ksh/sound_pred.py
+++ x_ksh/sound_pred.py
@@ -1,47 +1,61 @@
 import tensorflow as tf
 import numpy as np
-path='/Users/kimseunghyuck/desktop/'
-opt_addr="/Users/kimseunghyuck/desktop/git/daegon/KYLius-method/PROJECT2/optx2/optx2"
+import pandas as pd
+
+#opt_addr="/Users/kimseunghyuck/desktop/git/daegon/KYLius-method/PROJECT2/optx2/optx2"
+#opt_addr="/Users/kimseunghyuck/desktop/git/daegon/KYLius-method/PROJECT2/optx/optx"
+#opt_addr="/Users/kimseunghyuck/desktop/git/daegon/KYLius-method/PROJECT2/one/optx"
 
 class sound_pred:
     def __init__(self, opt_addr):
         # initialize/ load
-        saver=tf.train.import_meta_graph(opt_addr+".meta")
-        sess = tf.InteractiveSession()
+        self.idx = np.array(['Hi-hat', 'Saxophone', 'Trumpet', 'Glockenspiel', 'Cello', 'Knock',
+       'Gunshot_or_gunfire', 'Clarinet', 'Computer_keyboard',
+       'Keys_jangling', 'Snare_drum', 'Writing', 'Laughter', 'Tearing',
+       'Fart', 'Oboe', 'Flute', 'Cough', 'Telephone', 'Bark', 'Chime',
+       'Bass_drum', 'Bus', 'Squeak', 'Scissors', 'Harmonica', 'Gong',
+       'Microwave_oven', 'Burping_or_eructation', 'Double_bass',
+       'Shatter', 'Fireworks', 'Tambourine', 'Cowbell', 'Electric_piano',
+       'Meow', 'Drawer_open_or_close', 'Applause', 'Acoustic_guitar',
+       'Violin_or_fiddle', 'Finger_snapping'])
+    
+        self.path='/Users/kimseunghyuck/desktop/'
+        self.saver=tf.train.import_meta_graph(opt_addr+".meta")
+        self.sess = tf.InteractiveSession()
         print("Meta_Graph Imported")
         
         # parameters save 
-        saver.restore(sess, opt_addr)
+        self.saver.restore(self.sess, opt_addr)
         print("Parameters Restored")
         
         # variables 
-        graph=tf.get_default_graph()
-        X=graph.get_tensor_by_name('X:0')
-        pred=graph.get_tensor_by_name('pred:0')
-        p_keep_conv=graph.get_tensor_by_name('p_keep_conv:0')
-        p_keep_hidden=graph.get_tensor_by_name('p_keep_hidden:0')
+        self.graph=tf.get_default_graph()
+        self.X=self.graph.get_tensor_by_name('X:0')
+        self.pred=self.graph.get_tensor_by_name('pred:0')
+        self.p_keep_conv=self.graph.get_tensor_by_name('p_keep_conv:0')
+        self.p_keep_hidden=self.graph.get_tensor_by_name('p_keep_hidden:0')
         print("Variables Saved")
     
     def tryit(self, soundaddr, method):
-        import pandas as pd
-        train = pd.read_csv('/Users/kimseunghyuck/desktop/sound_train.csv')
-        idx = train.label.unique()        
         import sys
         sys.path.append('/Users/kimseunghyuck/desktop/git/daegon/KYLius-method/x_ksh')        
         if method=="m":
             from mfcc import five_sec_extract
-            testfile8 = pd.read_csv('/Users/kimseunghyuck/desktop/testfile8.csv')
-            testfile8 = np.array(testfile8).reshape(-1)
-            testLabel8 = np.genfromtxt('/Users/kimseunghyuck/desktop/testLabel8.csv', delimiter=',')
+            #testfile8 = pd.read_csv('/Users/kimseunghyuck/desktop/testfile8.csv')
+            test = pd.read_csv(self.path+soundaddr)
+            test = np.array(test).reshape(-1)
+            testLabel = np.genfromtxt(self.path+'testLabel8.csv', delimiter=',')
             error=0
             k=0
             errorlist1=[]
-            for file in testfile8:
+            for file in test:
                 #mfcc processing
-                mfcc=five_sec_extract('/Users/kimseunghyuck/desktop/audio_train/'+file)
+                mfcc=five_sec_extract(self.path+'audio_train/'+file)
                 #classification result
-                result=sess.run(pred, feed_dict={X: mfcc.reshape(1, 20, 430), p_keep_conv: 1.0, p_keep_hidden: 1.0})
-                if testLabel8[k] != result:
+                result=self.sess.run(self.pred, feed_dict={self.X: mfcc.reshape(1, 20, 430), 
+                                                      self.p_keep_conv: 1.0, 
+                                                      self.p_keep_hidden: 1.0})
+                if testLabel[k] != result:
                     print('x : ', file)
                     error+=1
                     errorlist1.append(file)
@@ -53,18 +67,21 @@ class sound_pred:
             
         elif method=='s':
             from stft import five_sec_extract2
-            testfile8 = pd.read_csv('/Users/kimseunghyuck/desktop/testfile8.csv')
-            testfile8 = np.array(testfile8).reshape(-1)
-            testLabel8 = np.genfromtxt('/Users/kimseunghyuck/desktop/testLabel8.csv', delimiter=',')
+            #testfile8 = pd.read_csv('/Users/kimseunghyuck/desktop/testfile8.csv')
+            test = pd.read_csv(self.path+soundaddr)
+            test = np.array(test).reshape(-1)
+            testLabel = np.genfromtxt(path+'testLabel8.csv', delimiter=',')
             error=0
             k=0
             errorlist2=[]
-            for file in testfile8:
+            for file in test:
                 #mfcc processing
-                stft=five_sec_extract2('/Users/kimseunghyuck/desktop/audio_train/'+file)
+                stft=five_sec_extract2(self.path+'audio_train/'+file)
                 #classification result
-                result=sess.run(pred, feed_dict={X: stft.reshape(1, 17, 200), p_keep_conv: 1.0, p_keep_hidden: 1.0})
-                if testLabel8[k] != result:
+                result=self.sess.run(pred, feed_dict={self.X: stft.reshape(1, 17, 200), 
+                                                 self.p_keep_conv: 1.0, 
+                                                 self.p_keep_hidden: 1.0})
+                if testLabel[k] != result:
                     print('x : ', file)
                     error+=1
                     errorlist2.append(file)
@@ -74,38 +91,63 @@ class sound_pred:
             print("error count :", error)
             print("error percentage :", (474-error)/474)    
 
-    def submission(self, folder):
-        import pandas as pd
-        train = pd.read_csv('/Users/kimseunghyuck/desktop/sound_train.csv')
-        idx = train.label.unique()        
+    def submission1(self, folder):  
         import sys
         sys.path.append('/Users/kimseunghyuck/desktop/git/daegon/KYLius-method/x_ksh')        
         import os
-        file_list=os.listdir(path+"audio_test")
+        file_list=os.listdir(self.path+"audio_test")
         file_list=np.array(file_list).reshape(-1)
         submission={}
-        
-            from stft import five_sec_extract2
-            for file in file_list:
-                if (file.split('.')[-1]=="wav") & (file not in ['0b0427e2.wav', '6ea0099f.wav', 'b39975f5.wav']):
-                    #mfcc processing
-                    stft=five_sec_extract2('/Users/kimseunghyuck/desktop/audio_test/'+file)
-                    #classification result
-                    result=sess.run(pred, feed_dict={X: stft.reshape(1, 17, 200), p_keep_conv: 1.0, p_keep_hidden: 1.0})
-                    submission[file]=idx[result[0]]
+        from mfcc import five_sec_extract
+        for file in file_list:
+            if (file.split('.')[-1]=="wav") & (file not in ['0b0427e2.wav', '6ea0099f.wav', 'b39975f5.wav']):
+                #mfcc processing
+                stft=five_sec_extract2(self.path+'audio_test/'+file)
+                #classification result
+                result=self.sess.run(self.pred, feed_dict={self.X: stft.reshape(1, 17, 200), 
+                                                      self.p_keep_conv: 1.0, 
+                                                      self.p_keep_hidden: 1.0})
+                submission[file]=self.idx[result[0]]
 
-len(submission)
-submission['0b0427e2.wav']=idx[0]
-submission['6ea0099f.wav']=idx[0]
-submission['b39975f5.wav']=idx[0]
-len(submission)
-KYLius2=pd.DataFrame([[k,v] for k,v in iter(submission.items())],columns=["fname","label"])
-KYLius2.to_csv(path+'KYLius2.csv', header=True, index=False, sep='\t')
+        len(submission)
+        submission['0b0427e2.wav']=self.idx[0]
+        submission['6ea0099f.wav']=self.idx[0]
+        submission['b39975f5.wav']=self.idx[0]
+        len(submission)
+        KYLius1=pd.DataFrame([[k,v] for k,v in iter(submission.items())],columns=["fname","label"])
+        KYLius1.to_csv(self.path+'KYLius1.csv', header=True, index=False, sep='\t')
 
+    def submission2(self, folder):  
+        import sys
+        sys.path.append('/Users/kimseunghyuck/desktop/git/daegon/KYLius-method/x_ksh')        
+        import os
+        file_list=os.listdir(self.path+"audio_test")
+        file_list=np.array(file_list).reshape(-1)
+        submission={}
+        from stft import five_sec_extract2
+        for file in file_list:
+            if (file.split('.')[-1]=="wav") & (file not in ['0b0427e2.wav', '6ea0099f.wav', 'b39975f5.wav']):
+                #mfcc processing
+                stft=five_sec_extract2(self.path+'audio_test/'+file)
+                #classification result
+                result=self.sess.run(self.pred, feed_dict={self.X: stft.reshape(1, 17, 200), 
+                                                      self.p_keep_conv: 1.0, 
+                                                      self.p_keep_hidden: 1.0})
+                submission[file]=self.idx[result[0]]
+
+        len(submission)
+        submission['0b0427e2.wav']=self.idx[0]
+        submission['6ea0099f.wav']=self.idx[0]
+        submission['b39975f5.wav']=self.idx[0]
+        len(submission)
+        KYLius2=pd.DataFrame([[k,v] for k,v in iter(submission.items())],columns=["fname","label"])
+        KYLius2.to_csv(self.path+'KYLius2.csv', header=True, index=False, sep='\t')
 
     def close(self):
-        sess.close()
+        self.sess.close()
 
+
+"""
 np.savetxt(path+'errorlist1.csv', 
            errorlist1, header = " ", fmt='%s')
         
@@ -115,14 +157,10 @@ np.savetxt(path+'errorlist2.csv',
 errorlist1 = pd.read_csv('/Users/kimseunghyuck/desktop/errorlist1.csv')
 errorlist2 = pd.read_csv('/Users/kimseunghyuck/desktop/errorlist2.csv')
 
-
-type(errorlist1)
-type(errorlist2)
-
 #교집합은 어떻게 되는가?
 intersect = [x for x in errorlist1.values.tolist() if x in errorlist2.values.tolist()]
 len([x for x in errorlist1.values.tolist() if x in errorlist2.values.tolist()])
 #49
 len(errorlist1.values.tolist())     #136
 len(errorlist2.values.tolist())     #101
-
+"""
